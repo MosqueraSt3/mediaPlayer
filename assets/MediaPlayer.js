a88ef6adf93370a5b8a028694715e1e2920e27c3
MosqueraSt3 <esmosquera66@misena.edu.co>
2021-01-01 23:03:15 -0500

    implementado modulo

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,19 @@
+class MediaPlayer {
+    constructor(config){
+        this.media = config.el
+    }
+
+    play(){
+        this.media.play()
+    }
+
+    pause(){
+        this.media.pause()
+    }
+
+    togglePlay(){
+        this.media.paused ? this.play() : this.pause()
+    } 
+}
+
+export default MediaPlayer
